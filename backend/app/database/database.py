c1db8e15e12d79c0c7f14ec929a6cec065b09fc0
kvbd176 <164621930+kvbd176@users.noreply.github.com>
2026-07-15 14:30:51 +0530

    Update database.py

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -1,6 +1,11 @@
+import os
+from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
@@ -13,7 +18,6 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
